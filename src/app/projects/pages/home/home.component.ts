975bd14ee6252dd5561dd22a402d93370c81d689
joris hermans <JHermans85@hotmail.com>
2023-10-06 14:09:25 +0200

    improve homepage

--- src/app/projects/pages/home/home.component.ts
+++ src/app/projects/pages/home/home.component.ts
@@ -2,14 +2,14 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'twm-projects',
+  selector: 'twm-home',
   template: `
     <ng-container>
       <div>
-        <h2> Welcom </h2>
-        This is all about creating code stories. This idea started off when I created video's for my youtube channel typewithme.
+        <h2> Welcome </h2>
+        <p>This is all about creating code stories. This idea started off when I created video's for my youtube channel typewithme.
         Creating and learning around javascript and typescript can be a hard thing todo. When preparing video's and learning about new technology you need a tool to guide you with that.
-        So this is the kind of tool I created for you.
+        So this is the kind of tool I created for you.</p>
         <div class="buttons">
             <a href="/create">Start your journey</a>
         </div>
